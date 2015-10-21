1466d1f7f4fb102dc42364105fbecbbbd6b20596
Lucas Magroski <magroski@gmail.com>
2015-10-21 21:20:48 -0200

    test db2

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
 	if (err) throw err;
 	console.log('Connected to postgres! Getting schemas...');
 	client
-    	.query('SELECT table_schema,table_name FROM information_schema.tables;')
+    	.query('SELECT * FROM reminders;')
     	.on('row', function(row) {
       		console.log(JSON.stringify(row));
     	});
