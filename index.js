4125a8d7f87a0dce5041120907151e483b6deb73
Lucas Magroski <magroski@gmail.com>
2015-10-21 10:16:42 -0200

    version 0.3

--- index.js
+++ index.js
@@ -41,39 +41,41 @@ slack.on('message', function(data) {
 				slack.sendMsg(data.channel, "`docs` Lista de comandos do Google Docs \n `help` Essa lista de comandos")
 				break;
 			case "docs":
-				slack.sendMsg(data.channel, "`Ctrl+F` Procura texto no arquivo \n
-											`Ctrl + Home  Receba de volta ao topo do seu documento \n
-											`Ctrl + B` Negrito \n
-											`Ctrl + E` alinhamento Centro \n
-											`Ctrl + L` Voltar para o alinhamento à esquerda \n
-											`Ctrl + M` Inserir comentário \n
-											`Ctrl + H` Substituir \n
-											`Ctrl + End ` Ir para a última célula na região de dados \n
-											`Ctrl + Home` Ir para a primeira célula na região de dados \n
-											`Shift + barra de espaço ` Selecione linha inteira \n
-											`Ctrl + barra de espaço ` Selecione coluna inteira \n
-											`Ctrl + Z ` Desfazer \n
-											`Ctrl + Y` Refazer \n
-											`Ctrl + J ` justificar \n
-											`Ctrl + Shift + L ` lista de marcadores \n
-											`Ctrl + Shift + Espaço` Inserir espaço sem quebras \n
-											`Page Down ` Mover uma tela abaixo \n
-											`Ctrl + Shift + F` Tela cheia \n
-											`Page Up ` Mover uma tela acima \n
-											`Ctrl + Espaço` Remover a formatação")
+				slack.sendMsg(data.channel, "`Ctrl+F` Procura texto no arquivo \n"+
+											"`Ctrl + Home  Receba de volta ao topo do seu documento \n"+
+											"`Ctrl + B` Negrito \n"+
+											"`Ctrl + E` alinhamento Centro \n"+
+											"`Ctrl + L` Voltar para o alinhamento à esquerda \n"+
+											"`Ctrl + M` Inserir comentário \n"+
+											"`Ctrl + H` Substituir \n"+
+											"`Ctrl + End ` Ir para a última célula na região de dados \n"+
+											"`Ctrl + Home` Ir para a primeira célula na região de dados \n"+
+											"`Shift + barra de espaço ` Selecione linha inteira \n"+
+											"`Ctrl + barra de espaço ` Selecione coluna inteira \n"+
+											"`Ctrl + Z ` Desfazer \n"+
+											"`Ctrl + Y` Refazer \n"+
+											"`Ctrl + J ` justificar \n"+
+											"`Ctrl + Shift + L ` lista de marcadores \n"+
+											"`Ctrl + Shift + Espaço` Inserir espaço sem quebras \n"+
+											"`Page Down ` Mover uma tela abaixo \n"+
+											"`Ctrl + Shift + F` Tela cheia \n"+
+											"`Page Up ` Mover uma tela acima \n"+
+											"`Ctrl + Espaço` Remover a formatação")
 				break;
 		}
 	}
 });
 
-var birthdays = ['lucas'=>'21/10'];
+var birthdays = [];
+birthdays['lucas'] = '21/10';
 
 slack.on('presence_change', function(data){
 	var currentTime = new Date();
 	var currentHour = currentTime.getHours();
+	var currentDate = currentTime.getDate()+'/'+(currentTime.getMonth()+1);
 	if(data.presence=='online' && currentHour < 12){
 		var userName = slack.getUser(data.user).name;
-		if( typeof birthdays[userName] != typeof undefined ){
+		if( typeof birthdays[userName] != typeof undefined && birthdays[userName] == currentDate ){
 			slack.sendMsg('general','Feliz aniversário @'+userName+' :cake:');
 		}
 	}
